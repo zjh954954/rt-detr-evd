48b50ab5440d0100976ace24e0cb2f29e1c54b67
zjh954954 <1143332789@qq.com>
2025-05-08 16:51:19 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,20 @@
+import warnings, os
+warnings.filterwarnings('ignore')
+from ultralytics import RTDETR
+
+
+
+if __name__ == '__main__':
+    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
+    # model.load('') # loading pretrain weights
+    model.train(data='dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=100,
+                batch=4,
+                workers=4, #
+                # device='0,1', # 
+                # resume='', # last.pt path
+                project='runs/train',
+                name='exp',
+                )
